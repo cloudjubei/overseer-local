7f6de116d97b020ed24ffad5f5d2167ea072e0ad
Kamil Chmurzynski <cloudjubei@gmail.com>
2025-09-16 22:20:17 +0200

    Fixes

--- src/renderer/components/tasks/ExclamationChip.tsx
+++ src/renderer/components/tasks/ExclamationChip.tsx
@@ -3,7 +3,7 @@ import { IconExclamation } from '../ui/Icons'
 export default function ExclamationChip({ title, tooltip }: { title: string; tooltip: string }) {
   return (
     <span className="rejection-badge" aria-label={tooltip} title={title}>
-      <IconExclamation className="w-4 h-4" />
+      <IconExclamation className="w-5 h-5" />
     </span>
   )
 }
